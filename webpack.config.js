32a98dd08c179e613abd2d844fd59b1d8101adf6
Diego Galarce <galarcediego@gmail.com>
2020-07-26 21:00:56 -0300

    probando las configuracion de webpack y la dependencias, img

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
  
 module.exports = {
   entry: {
@@ -16,6 +17,17 @@ module.exports = {
       {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+                name: 'img/[name].[ext]',
+             },
+          },
+        ],
       }
 
     ]
@@ -134,7 +146,17 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
         filename: 'css/style.css'
+    }),
+    new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: './src/img',
+            to: 'img',
+          },
+         
+        ]
     })
+
 ],
 };
 
