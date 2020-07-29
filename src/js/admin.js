b053db529132e36d4a3a4c18d4aa7d8ce461bfb3
Daiana Olmi <dai-olmi@hotmail.com>
2020-07-28 22:06:00 -0300

    realice maquetado de la lista y pagina csss

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '../css/style.css'
+import '@fortawesome/fontawesome-free/js/all.min.js'
 
 console.log("si funciona admin");
