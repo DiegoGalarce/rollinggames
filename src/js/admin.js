271d8edf8b9245a9af4de7774a62cc4892621580
Diego Galarce <galarcediego@gmail.com>
2020-08-01 18:56:02 -0300

    ya falta poco para terminar los conflictos de las paginas contc registro

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,10 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
-<<<<<<< HEAD
 import '../css/style.css'
 import '@fortawesome/fontawesome-free/js/all.min.js'
 console.log("si funciona admin");
-=======
-import '../css/style.css';
-import '@fortawesome/fontawesome-free/js/all.min.js';
->>>>>>> paginaRegistro
