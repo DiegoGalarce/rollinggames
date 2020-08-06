a347175e14e443c72e4119e31df56c328c1fbcf6
Mauricio G. Castro <mauriciogabrielcastro@gmail.com>
2020-08-06 01:52:02 -0300

    las imagenes quedan chicas en las cards - error en innerHTML

--- src/js/infoJuegos.js
+++ src/js/infoJuegos.js
@@ -12,21 +12,21 @@ function leerJuegos() {
         for (let i in listaJuegos) {
 
             let codHtml = `<article class="" id="${listaJuegos[i].codigo}">
-                       <div class="col-sm-12 col-md-3 d-flex justify-content-between juegoInicio my-3">
+            
+                       <div class="col-sm-12 col-md-3 col-md-3 d-flex justify-content-between juegoInicio my-3">
                    <div class="card borde">
                            <img class="card-img-top" src="img/${listaJuegos[i].imagen}"
                                alt="${listaJuegos[i].nombre}">
                           <div class="card-body text-center cajaCategoria">
                             <h5 class="titulo">${listaJuegos[i].nombre}</h5>
-                             <a href="#" class="btn botonredondeado azul2 px-5 text-light shadow">Ver más</a>
+                             <a href="infoJuegos.html" class="btn botonredondeado azul2 px-5 text-light shadow">Ver más</a>
                              </div>
                     </div>
                  </div>
                 </article>`;
-            
+
             switch (listaJuegos[i].categoria) {
                 case "Acción":
-                   
                     let catalogoAccion = document.getElementById("catalogoAccion");
                     catalogoAccion.innerHTML += codHtml;
                     break;
