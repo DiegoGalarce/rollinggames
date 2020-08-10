31cb65f3681162267e5c055362de3bec8420c4d5
Diego Galarce <galarcediego@gmail.com>
2020-08-09 21:37:47 -0300

    se genera un actualizacion de la pagina admin en la rama dev

--- src/js/admin.js
+++ src/js/admin.js
@@ -5,42 +5,99 @@ import Juego from './juego.js'
 import $ from 'jquery'
 import '@fortawesome/fontawesome-free/js/all.min.js'
 
-let listaProductos = [];
-leerProductos();
+let listaJuegos = [];
+leerJuegos();
 
 let juegoExistente = false;
 
-window.agregarProducto = function agregarProducto() {
+window.agregarJuego = function agregarJuego() {
 
-    console.log("desde funcion agregar producto")
     let codigo = document.getElementById('codigo').value;
+    document.getElementById('codigo').removeAttribute("readonly");
     let nombre = document.getElementById('nombre').value;
     let categoria = document.getElementById('categoria').value;
     let descripcion = document.getElementById('descripcion').value;
     let imagen = document.getElementById('imagen').value;
 
     //validar general de todos los datos
-    let productoNuevo = new Juego(codigo, nombre, categoria, descripcion, imagen)
-    console.log("productoNuevo");
 
-    listaProductos.push(productoNuevo);
+    console.log(document.getElementById('a'));
+    let juegoNuevo = new Juego(codigo, nombre, categoria, descripcion, imagen);
 
-    localStorage.setItem('juegoKey', JSON.stringify(listaProductos));
-    //mostrar el arreglo
-    leerProductos();
+    listaJuegos.push(juegoNuevo);
+
+    localStorage.setItem('juegoKey', JSON.stringify(listaJuegos));
+
+    leerJuegos();
+
     limpiarFormulario();
-    //cerrar ventana modal
+
     let ventanaModal = document.getElementById('modal');
     $(ventanaModal).modal('hide');
 
 };
 
-function leerProductos() {
+window.validarGeneral = function (event) {
+    event.preventDefault();
+    console.log("Desde validar general");
+
+    if (validarCodigo(document.getElementById('codigo')) &&
+        validarNombre(document.getElementById('nombre')) &&
+        validarCategoria(document.getElementById('categoria')) &&
+        validarDescripcion(document.getElementById('descripcion'))) {
+            decidir();
+    } else {
+        alert("Hay un error en el formulario")
+    }
+}
+
+window.validarCodigo = function (input) {
+    if (input.value == "") {
+        input.className = "form-control is-invalid"
+        return false;
+    } else {
+        input.className = "form-control is-valid"
+        return true;
+    }
+}
+
+window.validarNombre = function (input) {
+    if (input.value == "") {
+        input.className = "form-control is-invalid"
+        return false;
+    } else {
+        input.className = "form-control is-valid"
+        return true;
+    }
+}
+
+window.validarCategoria = function (input) {
+    if (input.value == "") {
+        input.className = "form-control is-invalid"
+        return false;
+    } else {
+        input.className = "form-control is-valid"
+        return true;
+    }
+}
+
+window.validarDescripcion = function (input) {
+    if (input.value == "") {
+        input.className = "form-control is-invalid"
+        return false;
+    } else {
+        input.className = "form-control is-valid"
+        return true;
+    }
+
+}
+
+function leerJuegos() {
     if (localStorage.length > 0) {
         let arregloJuego = JSON.parse(localStorage.getItem('juegoKey'));
 
-        if (listaProductos.length == 0) {
-            listaProductos = arregloJuego;
+        if (listaJuegos.length == 0) {
+            listaJuegos = arregloJuego;
         }
         //borrar filas de la tabla
         borrarFilas();
@@ -59,12 +116,12 @@ function dibujarFilas(arregloJuego) {
     <td>${arregloJuego[i].descripcion}</td>
     <td>${arregloJuego[i].imagen}</td>
     <td>
-        <button class="btn btn-outline-success" id="publicado" onclick="marcarPublicado(1)"><i class="fas fa-check"></i></button>
+        <button class="btn botontabla" id="publicado" onclick="marcarPublicado(event)"><i class="fas fa-check" style="color: #C8C8C5"></i></button>
     </td>
     <td>
         <button class="btn btn-danger"><i class="fas fa-trash-alt" onclick="eliminarJuego(${arregloJuego[i].codigo})"></i></button>
         <button class="btn btn-primary"><i class="far fa-edit" onclick="prepararJuego(${arregloJuego[i].codigo})"></i></button>
-        <button class="btn btn-outline-warning" id="favorito" onclick="marcarFavorito(1)"><i class="far fa-star"></i></button>
+        <button class="btn botontabla" id="btn" onclick="destacarJuego(event)"><i class=" far fa-star" style="color: #C8C8C5"></i></button>
     </td>
 </tr>`
 
@@ -88,21 +145,22 @@ window.limpiarFormulario = function () {
 }
 
 window.eliminarJuego = function (codigo) {
-    let arregloFiltrado = listaProductos.filter(function (juego) {
+    let arregloFiltrado = listaJuegos.filter(function (juego) {
         return juego.codigo != codigo;
     });
     localStorage.setItem("juegoKey", JSON.stringify(arregloFiltrado))
-    listaProductos = arregloFiltrado;
+    listaJuegos = arregloFiltrado;
 
-    leerProductos();
+    leerJuegos();
 }
-    
+
 window.prepararJuego = function (codigo) {
-    
-    let juegoEncontrado = listaProductos.find(function (producto) {
+
+    let juegoEncontrado = listaJuegos.find(function (producto) {
         return producto.codigo == codigo;
     })
     document.getElementById('codigo').value = juegoEncontrado.codigo;
+    document.getElementById('codigo').setAttribute("readonly","true");
     document.getElementById('nombre').value = juegoEncontrado.nombre;
     document.getElementById('categoria').value = juegoEncontrado.categoria;
     document.getElementById('descripcion').value = juegoEncontrado.descripcion;
@@ -114,10 +172,9 @@ window.prepararJuego = function (codigo) {
     juegoExistente = true;
 }
 
-window.decidir = function (event) {
-    event.preventDefault();
+window.decidir = function () {
     if (juegoExistente == false) {
-        agregarProducto();
+        agregarJuego();
     } else {
         modificarProducto();
     }
@@ -129,42 +186,35 @@ function modificarProducto() {
     let categoria = document.getElementById('categoria').value;
     let descripcion = document.getElementById('descripcion').value;
     let imagen = document.getElementById('imagen').value;
-    
-    for (let i in listaProductos) {
-        if (listaProductos[i].codigo == codigo) {
-            listaProductos[i].nombre = nombre;
-            listaProductos[i].categoria = categoria;
-            listaProductos[i].descripcion = descripcion;
-            listaProductos[i].imagen = imagen;
+
+    for (let i in listaJuegos) {
+        if (listaJuegos[i].codigo == codigo) {
+            listaJuegos[i].nombre = nombre;
+            listaJuegos[i].categoria = categoria;
+            listaJuegos[i].descripcion = descripcion;
+            listaJuegos[i].imagen = imagen;
         }
     }
-    
-    localStorage.setItem("juegoKey", JSON.stringify(listaProductos));
-    leerProductos();
+    localStorage.setItem("juegoKey", JSON.stringify(listaJuegos));
+    leerJuegos();
     limpiarFormulario();
-    
+
     let ventanaModal = document.getElementById('modal');
     $(ventanaModal).modal('hide');
 }
 
-// window.marcarFavorito = function(x){
-//     var x;
-//     if(x == 1){
-//         contador = contador +1;
-//     }
-//     if(contador == 1){
-//         document.getElementById('favorito').style.background="#F3F325"
-//     }else if(contador == 2){
-//         document.getElementById('favorito').style.background="";
-//         contador = 0;
-//     }
-// }
+window.destacarJuego = function (e) {
+    var target = e.target,
+        count = +target.dataset.count;
 
-$("#favorito").click(function(){
-    $(this).toggleClass(" btn-warning");
-});
+    target.style.backgroundColor = count === 1 ? "#F3F3F1" : '#FFFF76';
+    target.dataset.count = count === 1 ? 0 : 1;
+}
 
-$("#publicado").click(function(){
-    $(this).toggleClass(" btn-success");
-});
+window.marcarPublicado = function (e) {
+    var target = e.target,
+        count = +target.dataset.count;
 
+    target.style.backgroundColor = count === 1 ? "#F3F3F1" : '#57D757';
+    target.dataset.count = count === 1 ? 0 : 1;
+}
